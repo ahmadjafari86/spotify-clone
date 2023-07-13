d6a57414f44d8dd13a8f9a9929144bb78a5db988
ahmadDev <ahmadjafari6050@gmail.com>
2023-07-13 16:35:35 +0330

    feat: header and list items

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,10 +11,20 @@ export default function Home() {
               Welcome to music
             </h1>
             <div className="gird grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-              <ListItem />
+              <ListItem
+                image="/images/liked.png"
+                name="Liked Songs"
+                href="Liked"
+              />
             </div>
           </div>
         </Header>
+        <div className="mt-2 mb-7 px-6">
+          <div className="flex items-center justify-between">
+            <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
+          </div>
+          <div>List of songs</div>
+        </div>
       </h2>
     </main>
   );
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,5 +1,35 @@
-const ListItem = () => {
-  return <div>ListItem</div>;
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
+
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
+}
+const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+  const router = useRouter();
+  const onClick = () => {
+    // check authentication before route change
+    router.push(href);
+  };
+  return (
+    <div>
+      <button
+        className="relative group flex items-center min-w-[255px] rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+        onClick={onClick}
+      >
+        <div className="relative min-h-[64px] min-w-[64px]">
+          <Image className="object-cover" fill src={image} alt="Image" />
+        </div>
+        <p className="font-medium truncate py-5">{name}</p>
+        <div className=" absolute flex items-center justify-center rounded-full md:opacity-0 transition bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+          <FaPlay className=" text-black" />
+        </div>
+      </button>
+    </div>
+  );
 };
 
 export default ListItem;
