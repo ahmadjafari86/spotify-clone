4f9e1a25aa8042c90a975b32254139859b02bf7d
ahmadDev <ahmadjafari6050@gmail.com>
2023-07-13 07:15:43 +0330

    feat: header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,21 @@
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
     <main>
-      <h2 className=' text-green-500'>spotify home</h2>
+      <h2 className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+        <Header>
+          <div className="mb-2">
+            <h1 className=" text-white text-3xl font-semibold">
+              Welcome to music
+            </h1>
+            <div className="gird grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+              <ListItem />
+            </div>
+          </div>
+        </Header>
+      </h2>
     </main>
-  )
+  );
 }
